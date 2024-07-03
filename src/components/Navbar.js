78707e630c3d0c8b7b6a0d2b1e9870840cab272e
RafaelPapa.dev <rafanicolasp@hotmail.com>
2024-07-03 09:40:24 -0300

    ajuste navbat

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,33 +13,35 @@ function Navbar() {
             <div className="navbar-logo">
                 <img src={logo} alt="Grupo Prodemol" />
             </div>
-            <ul className="navbar-links">
-                <li>
-                    <Link to="/" className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}>Home</Link>
-                </li>
-                <li>
-                    <Link to="/empresa" className={`navbar-link ${location.pathname === '/empresa' ? 'active' : ''}`}>Empresa</Link>
-                </li>
-                <li>
-                    <Link to="/servicos" className={`navbar-link ${location.pathname === '/servicos' ? 'active' : ''}`}>Serviços</Link>
-                </li>
-                <li>
-                    <Link to="/produtos" className={`navbar-link ${location.pathname === '/produtos' ? 'active' : ''}`}>Produtos</Link>
-                </li>
-                <li>
-                    <Link to="/tecnologia" className={`navbar-link ${location.pathname === '/tecnologia' ? 'active' : ''}`}>Tecnologia</Link>
-                </li>
-                <li>
-                    <Link to="/qualidade" className={`navbar-link ${location.pathname === '/qualidade' ? 'active' : ''}`}>Qualidade</Link>
-                </li>
-                <li>
-                    <Link to="/contato" className={`navbar-link ${location.pathname === '/contato' ? 'active' : ''}`}>Contato</Link>
-                </li>
-            </ul>
-            <div className="navbar-whatsapp">
-                <a href="https://wa.me/yourwhatsappnumber" target="_blank" rel="noopener noreferrer">
-                    <img src={whatsapp} alt="WhatsApp" />
-                </a>
+            <div className='nav-right'>
+                <ul className="navbar-links">
+                    <li>
+                        <Link to="/" className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/empresa" className={`navbar-link ${location.pathname === '/empresa' ? 'active' : ''}`}>Empresa</Link>
+                    </li>
+                    <li>
+                        <Link to="/servicos" className={`navbar-link ${location.pathname === '/servicos' ? 'active' : ''}`}>Serviços</Link>
+                    </li>
+                    <li>
+                        <Link to="/produtos" className={`navbar-link ${location.pathname === '/produtos' ? 'active' : ''}`}>Produtos</Link>
+                    </li>
+                    <li>
+                        <Link to="/tecnologia" className={`navbar-link ${location.pathname === '/tecnologia' ? 'active' : ''}`}>Tecnologia</Link>
+                    </li>
+                    <li>
+                        <Link to="/qualidade" className={`navbar-link ${location.pathname === '/qualidade' ? 'active' : ''}`}>Qualidade</Link>
+                    </li>
+                    <li>
+                        <Link to="/contato" className={`navbar-link ${location.pathname === '/contato' ? 'active' : ''}`}>Contato</Link>
+                    </li>
+                </ul>
+                <div className="navbar-whatsapp">
+                    <a href="https://wa.me/yourwhatsappnumber" target="_blank" rel="noopener noreferrer">
+                        <img src={whatsapp} alt="WhatsApp" />
+                    </a>
+                </div>
             </div>
         </nav>
     );
