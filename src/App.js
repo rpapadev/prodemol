c3886855d4ad13fd0b04e8d7cc8eeee5bdc88b90
RafaelPapa.dev <rafanicolasp@hotmail.com>
2024-07-11 10:40:04 -0300

    scrolltotop adc

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import ScrollToTop from './components/ScrollToTop';
+
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
@@ -16,6 +18,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
